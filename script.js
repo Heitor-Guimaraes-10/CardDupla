44d8f566683d84605c902b3b480fde95048ee188
Guimarães <Guimaraesheitor.10.10@gmail.com>
2026-02-13 17:08:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,58 @@
+// Tema
+document.getElementById("theme-btn").addEventListener("click", () => {
+  document.body.classList.toggle("light");
+});
 
-// Seleciona elementos
-const btnSkills1 = document.getElementById("btn-skills1");
+// Card 1
+const btnSobre1 = document.getElementById("btn-sobre1");
+const btnHab1 = document.getElementById("btn-habilidades1");
 const btnContato1 = document.getElementById("btn-contato1");
-const btnSkills = document.getElementById("btn-skills");
-const btnContato = document.getElementById("btn-contato");
 
+const sobre1 = document.getElementById("sobre1");
 const skills1 = document.getElementById("skills1");
 const contato1 = document.getElementById("contato1");
-const skills = document.getElementById("skills");
-const contato = document.getElementById("contato");
 
-// Evento botão skills
-btnSkills1.addEventListener("click", function () {
-    skills1.classList.toggle("hidden");
-    contato1.classList.add("hidden");
+btnSobre1.addEventListener("click", () => {
+  sobre1.classList.toggle("hidden");
+  skills1.classList.add("hidden");
+  contato1.classList.add("hidden");
 });
 
-// Evento botão contato
-btnContato1.addEventListener("click", function () {
-    contato1.classList.toggle("hidden");
-    skills1.classList.add("hidden"); // fecha skills se estiver aberto
+btnHab1.addEventListener("click", () => {
+  skills1.classList.toggle("hidden");
+  sobre1.classList.add("hidden");
+  contato1.classList.add("hidden");
 });
 
-btnSkills.addEventListener("click", function () {
-    skills.classList.toggle("hidden");
-    contato.classList.add("hidden");
+btnContato1.addEventListener("click", () => {
+  contato1.classList.toggle("hidden");
+  sobre1.classList.add("hidden");
+  skills1.classList.add("hidden");
 });
 
-btnContato.addEventListener("click", function () {
-    contato.classList.toggle("hidden");
-    skills.classList.add("hidden"); // fecha skills se estiver aberto
+// Card 2
+const btnSobre2 = document.getElementById("btn-sobre");
+const btnHab2 = document.getElementById("btn-habilidades");
+const btnContato2 = document.getElementById("btn-contato");
+
+const sobre2 = document.getElementById("sobre2");
+const skills2 = document.getElementById("skills");
+const contato2 = document.getElementById("contato2");
+
+btnSobre2.addEventListener("click", () => {
+  sobre2.classList.toggle("hidden");
+  skills2.classList.add("hidden");
+  contato2.classList.add("hidden");
+});
+
+btnHab2.addEventListener("click", () => {
+  skills2.classList.toggle("hidden");
+  sobre2.classList.add("hidden");
+  contato2.classList.add("hidden");
+});
+
+btnContato2.addEventListener("click", () => {
+  contato2.classList.toggle("hidden");
+  sobre2.classList.add("hidden");
+  skills2.classList.add("hidden");
 });
